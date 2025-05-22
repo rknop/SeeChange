56cae53d9e80787dc02b4a994a2dcd05920b0dae
Rob Knop <raknop@lbl.gov>
2025-05-22 07:28:21 -0700

    mostly doc cleanup

--- pipeline/exposure_processor.py
+++ pipeline/exposure_processor.py
@@ -244,7 +244,6 @@ class ExposureProcessor:
             if self.numprocs > 1:
                 SCLogger.info( f"Creating pool of {self.numprocs} processes to do {len(chips)} chips" )
                 with multiprocessing.pool.Pool( self.numprocs, maxtasksperchild=1 ) as pool:
-                    pool.daemon = False
                     for chip in chips:
                         pool.apply_async( self.processchip, ( chip, ), {}, self.collate )
 
